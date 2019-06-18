0b4938a6a902c631920d1fb07799b56c455ec331
KamranJabiyev <49950252+KamranJabiyev@users.noreply.github.com>
2019-06-18 12:54:48 +0400

    Add files via upload

---
+++ 18.06.2019/DataType/DataType/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace DataType
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            sbyte age = 127;
+            age++;
+            char letter = 'l';
+            string str = "aynur";
+            bool t = false;
+            float d = 3.54F;
+            decimal x = 1.578855M;
+            //for (int i = 0; i < str.Length; i++)
+            //{
+            //    Console.WriteLine(str[i]);
+            //}
+            //foreach (var let in str)
+            //{
+            //    Console.WriteLine(let);
+            //}
+            //if (t)
+            //{
+            //    Console.WriteLine("yes");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("No");
+            //}
+            switch (str)
+            {
+                case "Shirvan":
+                    Console.WriteLine("Yes Shirvan");
+                    break;
+                case "Aynur":
+                    Console.WriteLine("Yes Shirvan2");
+                    break;
+                default:
+                    Console.WriteLine("Shirvan bu yasha catmaz: " + age);
+                    break;
+            }
+            
+            Console.ReadLine();
+        }
+    }
+}
---
+++ 18.06.2019/P111.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace P111{
+    public class Program{
+        public static void Main(){
+            string name="Shirvan";
+            Console.WriteLine("Hello "+name);
+            Console.ReadKey();
+        }
+    }
+}
